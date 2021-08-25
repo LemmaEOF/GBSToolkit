4b3f12acf026a8f127323797264f5b4ff357d578
Meredith Espinosa <6722691+LemmaEOF@users.noreply.github.com>
2021-08-25 01:30:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# GBS Toolkit
+ *Various Python tools for messing around with GB Studio projects.*
+ 
+GBS Toolkit is a few tools I've built while working on Soul and Silicon, a
+queer/trans dating simulator I'm writing for the GameBoy Color. It's a very
+text-heavy game, and while GB Studio has a dialogue review section, it can't
+always easily handle lots of branching dialogue, and there's no way for it to
+add new dialogue. **This project is a work in progress.**
+
+## Current Features
+- Dump all the dialogue in your project into separate text files for review
+
+## Planned Features
+- Import dialogue in a text file into the project JSON
+- Dumping and editing variable names.
+- Alternative visual text editor interface with automatic dialogue previews.
+
+## Requirements
+- Tested with GB Studio v2.0 beta 5. Other versions may not work properly.
+- Python 3.5 or higher.
+
+## Licensing and Contribution
+Contributions are more than welcome, and GBS Toolkit is publicly available
+under [FAFOL 0.2](LICENSE.md). You can use, modify, and even redistribute it
+however you want, as long as you're not being exploitative with it. Thank you
+for giving GBS Toolkit a look!
---
+++ gbstoolkit/textdumper.py
@@ -0,0 +1,178 @@
+import json
+import os
+import shutil
+
+sprites = {}  # uuid-to-readable-name key for avatar names TODO: somehow move into function?
+available_actors = {}  # uuid-to-readable-name key for actors TODO: somehow move into function?
+emotes = ["shock", "question", "heart", "pause", "angry", "sweat", "note", "sleep"]  # the built-in emote bubbles
+
+
+def dump_text(project_file: str):
+    """
+    Grabs all the text (and some events) from a GB Studio project
+    and exports it into files per scene, actor, and trigger.
+    :param project_file: A string path to the .gbsproj or .json file for the GB Studio project.
+    """
+    # volatiles for determining paths
+    current_scene = ""  # scene name, used for path
+    current_name = ""  # actor or trigger name, used for file name
+
+    # important stuff for the whole process
+    # TODO: don't have to edit the file to find your .gbsproj
+    project = open(project_file)
+    contents = json.load(project)  # the parsed JSON
+
+    sprite_sheets = contents["spriteSheets"]
+
+    if os.path.exists("out"):
+        try:
+            shutil.rmtree("out")
+        except OSError as e:
+            print("Error: %s : %s" % ("out", e.strerror))
+    os.makedirs("out")
+
+    with open("out/sprites.txt", mode="w") as out:
+        outcont = ""
+        for i in sprite_sheets:
+            if i["numFrames"] == 1:
+                sprites[i["id"]] = i["name"]
+                outcont = outcont + i["name"] + " (" + i["id"] + ")\n"
+        out.write(outcont)
+
+    scenes = contents["scenes"]
+    for scene in scenes:
+        current_scene = scene["name"]
+
+        # Dump scene init text
+        script = scene["script"]
+        contents = []
+        for event in script:
+            iter_event(event, contents, current_scene + " init")
+        if len(contents) > 0:
+            file_path = "out/scripts/" + current_scene
+            if not os.path.exists(file_path):
+                os.makedirs(file_path)
+            with open(file_path + "/init.txt", mode="w", encoding="utf-8") as file:
+                file.write("".join(contents))
+
+        # Dump actor text
+        actors = scene["actors"]
+        # TODO: this sucks but I have to iterate through two separate times
+        #  bc one is deep & I need everything prepped early
+        available_actors.clear()
+        for actor in actors:
+            name = actor["name"]
+            if name == "":
+                name = "Actor " + str(actors.index(actor))
+            available_actors[actor["id"]] = name
+        for actor in actors:
+            current_name = actor["name"].replace('/', '-').replace('\\', '-').replace(':', '-')
+            if current_name == "":
+                current_name = "Actor " + str(actors.index(actor))
+            contents = []
+            script_contents = []
+            script = actor["script"]
+            if len(script) > 0:
+                for event in script:
+                    iter_event(event, script_contents, current_scene + " actor " + current_name + " interact")
+                if len(script_contents) > 0:
+                    contents.append("# On interact: \n\n")
+                    contents.extend(script_contents)
+            start_contents = []
+            start_script = actor["startScript"]
+            if len(start_script) > 0:
+                for event in start_script:
+                    iter_event(event, start_contents, current_scene + " actor " + current_name + " init")
+                if len(start_contents) > 0:
+                    contents.append("# On initialize: \n\n")
+                    contents.extend(start_contents)
+            if len(contents) > 0:
+                file_path = "out/scripts/" + current_scene + "/" + "actors"
+                if not os.path.exists(file_path):
+                    os.makedirs(file_path)
+                with open(file_path + "/" + current_name + ".txt", mode="w", encoding="utf-8") as file:
+                    file.write("".join(contents))
+
+        # Dump trigger text
+        triggers = scene["triggers"]
+        for trigger in triggers:
+            current_name = trigger["name"].replace('/', '-').replace('\\', '-').replace(':', '-')
+            if current_name == "":
+                current_name = "Trigger " + str(triggers.index(trigger))
+            contents = []
+            script = trigger["script"]
+            for event in script:
+                iter_event(event, contents, current_scene + " trigger " + current_name, 0)
+            if len(contents) > 0:
+                file_path = "out/scripts/" + current_scene + "/" + "triggers"
+                if not os.path.exists(file_path):
+                    os.makedirs(file_path)
+                with open(file_path + "/" + current_name + ".txt", mode="w", encoding="utf-8") as file:
+                    file.write("".join(contents))
+    project.close()
+
+
+def iter_event(event: dict, contents: list, location: str, current_depth: int):
+    global sprites, available_actors, emotes
+    if "children" in event:  # text events can't have children so safe to go deeper
+        # TODO: mark that the node happens? might be important for branches
+        has_text = False
+        header_point = len(contents)
+        children = event["children"]
+        current_depth += 1
+        for k, v in children.items():
+            before_point = len(contents)
+            for e in v:
+                iter_event(e, contents, location, current_depth)
+            if len(contents) != before_point:
+                if not has_text:
+                    contents.insert(header_point, current_depth * "  " + "## Branch Condition, depth " +
+                                    str(current_depth) + "\n\n")
+                    before_point += 1
+                    has_text = True
+                contents.insert(before_point, current_depth * "  " + "### Case " + k + "\n\n")
+        if len(contents) != header_point:
+            contents.append(current_depth * "  " + "--End branch condition, depth " + str(current_depth) + "\n\n")
+    elif event["command"] == "EVENT_ACTOR_EMOTE":
+        args = event["args"]
+        actor_id = args["actorId"]
+        actor = ""
+        if actor_id == "player":
+            actor = "player"
+        elif actor_id == "$self$":
+            actor = "self"
+        elif actor_id in available_actors:
+            actor = available_actors[actor_id]
+        else:
+            print("ERROR parsing emote in " + location + ": Missing actor for UUID " + actor_id +
+                  "! Make sure the actor isn't null in GB Studio!")
+            actor = "NULL"
+        contents.append("<" + actor + " " + emotes[int(args["emoteId"])] + ">\n\n")
+    elif event["command"] == "EVENT_TEXT":
+        args = event["args"]
+        avatar = ""
+        if "avatarId" in args:  # for some reason this can just. not exist???
+            if args["avatarId"] != "":
+                actor_id = args["avatarId"]
+                if actor_id not in sprites:
+                    print("ERROR parsing dialogue in " + location + ": Missing avatar name for UUID " + actor_id +
+                          "! Make sure the sprite isn't null in GB Studio!")
+                    print("Errored dialogue: '''" + str(args["text"]) + "'''")
+                    contents.append("NULL:\n")
+                    avatar = "NULL"
+                else:
+                    contents.append(sprites[actor_id] + ":\n")
+                    avatar = sprites[actor_id]
+        text = args["text"]
+        if isinstance(text, list):
+            for t in text:
+                if text.index(t) != 0 and avatar != "":
+                    contents.append(avatar + ":\n")
+                contents.append(t.replace('Â…', '…'))
+                contents.append("\n\n")
+        else:
+            contents.append(text.replace('Â…', '…'))
+            contents.append("\n\n")
+        # TODO: mark other sorts of events - moves, shows/hides, etc.?
+        # TODO: other 1252-to-utf8 conversions because we. live. in. hell.
+
